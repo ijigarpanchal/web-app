04213f63d218dcaac57d15b221db47fb84161849
Jigar Panchal <50842855+ijigarpanchal@users.noreply.github.com>
2019-11-21 16:21:58 -0500

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: ' us-east-1_A8Oazr3eL', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6b523td5ceu7a1hsv7l555qnol', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    }
+};
